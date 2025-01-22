164d429e1ade9f44edfe434a7ab8cebf966446d2
manoula-hii <fethallahmanell@gmail.com>
2025-01-22 12:56:13 +0100

    ajouter des espaces

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,15 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+
+
+
+
+
+
+
+
+  
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
