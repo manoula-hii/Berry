98870a52ce2e2f85337f8be20b15c045cc03ac0a
manoula-hii <fethallahmanell@gmail.com>
2025-01-24 15:47:46 +0100

    raim

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,39 +1,79 @@
-import React from 'react'
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Image } from 'react-native';
-import images from '@/constants/images';
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const SingIn = () => {
-    const handleLogin = () => {};
-    return (
-        <SafeAreaView className="bg-white h-full">
-            <ScrollView contentContainerClassName="h-full">
-            <Image source={images.sculpture} className="w-2/4 h-1/2 self-center"  resizeMode="contain" />
+import { login } from "@/lib/appwrite";
+import { Redirect } from "expo-router";
+import { useGlobalContext } from "@/lib/global-provider";
+import images from "@/constants/images";
 
-            <View className ="px-10">
-                <Text className="text-3xl text-center uppercase font-rubik-bold text-black-default">
-                    Welcome to {"\n"}
-                    <Text>Berry !</Text>
-                </Text>
+const Auth = () => {
+  const { refetch, loading, isLogged } = useGlobalContext();
 
-                <Text className="text-base text-black-200 font-rubik-semibold text-center mt-2">
-                    Let's get you started.
-                </Text>
+  if (!loading && isLogged) return <Redirect href="/" />;
 
-                <Text className="text-lg font-rubik text-black-default text-center mt-16">Login to Berry with Google</Text>
-                <TouchableOpacity onPress={handleLogin} className ="bg-white shadow-md shadow-black-200 rounded-full w-full py-4 mt-5">
-                    <View className ="flex flex-row items-centre justify-center">
-                        <Image 
-                    source={images.google} className="w-5 h-7" resizeMode="contain"/>
-                    <Text className="text-lg font-rubik-midium text-black-200 ml-2 text-center"> Continue with Google </Text>
-                    </View>
-                </TouchableOpacity>
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to login");
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <Image
+          source={images.sculpture}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To Real Scout
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Real Scout with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={images.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
             </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-
-export default SingIn
+export default Auth;
